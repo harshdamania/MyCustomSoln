d8605147079dc42ba1a8751da48a00143f43b7ea
Your Name <you@example.com>
2018-04-20 20:33:19 +0530

    new Cod

---
+++ src/webparts/settings/SettingsWebPart.ts
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'SettingsWebPartStrings';
+import Settings from './components/Settings';
+import { ISettingsProps } from './components/ISettingsProps';
+
+export interface ISettingsWebPartProps {
+  description: string;
+}
+
+export default class SettingsWebPart extends BaseClientSideWebPart<ISettingsWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ISettingsProps > = React.createElement(
+      Settings,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}

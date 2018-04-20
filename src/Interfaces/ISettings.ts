96a94d676ad67f4275ecb749f947873f0af8fce2
Your Name <you@example.com>
2018-04-20 20:31:57 +0530

    Added ISettings interface

---
+++ src/Interfaces/ISettings.ts
@@ -0,0 +1,10 @@
+export interface ISettings{
+    ID:number;
+    Title:string;
+    Application:string;
+    Key:string;
+    Business_x0020_Unit:{
+        Title:string;
+        ID:Int16Array
+    }[];
+}
